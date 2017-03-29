59a8e5a0db6765cf28708bdf175ade470b86460f
ravikanttyagi4all <ravi.py4all@gmail.com>
2017-03-29 17:35:15 +0530

    Python

---
+++ 01-Day/02-BasicPrograms.py
@@ -0,0 +1,22 @@
+##a = 10
+##b = 20
+##c = a+b
+##
+##print(c)
+
+
+##a = "Hello"
+##b = "world"
+##
+##c = a+b
+##print(c*5)
+##
+##a = "10"
+##b = "20"
+##
+##print(int(a)+int(b))
+
+for i in range(0,6):
+    for j in range(1,i+1):
+        print("*",end="")
+    print()
---
+++ 01-Day/03-Strings.py
@@ -0,0 +1,12 @@
+a = "Python"
+
+print(a[1])
+
+print(a[0:3])
+
+print(a[-1])
+
+print(a[::-1])
+
+print(len(a))
+
